acb40e662e6aa2bd104994fb1924ee549056a757
saskatchuwan <kathryn.chu10@gmail.com>
2019-01-29 14:12:43 -0800

    Add Cat model with validations and #age

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -13,4 +13,29 @@
 #
 
 class Cat < ApplicationRecord
+  COLORS = %w(blue red yellow green brown lilac pokadot)
+  GENDER = %w(M F)
+
+  validates :birth_date, :name, presence: true
+  validates :color, presence: true, inclusion: { in: COLORS, message: "%{value} is not a valid color"}
+  validates :sex, presence: true, inclusion: { in: GENDER, message: "%{value} is not a valid gender (M or F)"}
+
+  def age
+    bday_month = birth_date.mon
+    bday_year = birth_date.year
+    bday_day = birth_date.mday
+
+    today = DateTime.now
+
+    if bday_month > today.mon
+      today.year - bday_year - 1
+    elsif bday_month < today.mon
+      today.year - bday_year
+    elsif bday_day > today.mday
+      today.year - bday_year - 1
+    else
+      today.year - bday_year
+    end
+
+  end
 end
