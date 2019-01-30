708018eb68ae1cb65e7ed6eca28b84765887d864
saskatchuwan <kathryn.chu10@gmail.com>
2019-01-29 17:27:05 -0800

    Add #update, #create and #edit to CatsController, add partial for new and edit views

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -15,7 +15,28 @@ class CatsController < ApplicationController
   end
 
   def create
+    @cat = Cat.new(cat_params)
 
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @cat = Cat.find_by(id: params[:id])
+    render :edit
+  end
+  
+  def update
+    @cat = Cat.find_by(id: params[:id])
+    
+    if @cat.update_attributes(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
   end
 
   private
